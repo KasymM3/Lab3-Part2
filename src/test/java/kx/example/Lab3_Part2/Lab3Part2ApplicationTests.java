8727600d38ef9a96d19c7e988f26b018c854acfb
KasymM3 <gaisin.k26@gmail.com>
2025-09-28 22:45:21 +0500

    V

---
+++ src/test/java/kx/example/Lab3_Part2/Lab3Part2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kx.example.Lab3_Part2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3Part2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
